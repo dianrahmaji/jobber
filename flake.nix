e0a20160b79630539226aa3fac74ebe392af2eac
Dian Rahmaji <dianrahmaji@gmail.com>
2025-05-29 16:44:13 +0700

    feat: intial setup

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Jobber project's flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nest-cli
+              nodejs_22
+              pnpm
+            ];
+          };
+        };
+      }
+    );
+}
