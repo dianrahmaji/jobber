051edbd3729276674eb14a6252fa6c2446bc847e
Dian Rahmaji <dianrahmaji@gmail.com>
2025-06-08 06:29:14 +0700

    feat: implemet 'GraphqlAuthGuard`

--- apps/jobber-auth/src/app/users/users.resolver.ts
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -2,16 +2,19 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './models/User.model';
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/CreateUser.input';
+import { UseGuards } from '@nestjs/common';
+import { GraphQLAuthGuard } from '../auth/guards/GraphQLAuth.guard';
 
 @Resolver(() => User)
 export class UsersResolver {
-  constructor(private readonly userService: UsersService) { }
+  constructor(private readonly userService: UsersService) {}
 
   @Mutation(() => User)
   async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
     return this.userService.createUser(createUserInput);
   }
 
+  @UseGuards(GraphQLAuthGuard)
   @Query(() => [User], { name: 'users' })
   async getUsers() {
     return this.userService.getUsers();
