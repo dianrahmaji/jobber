582c4d6cff813eea64c4cb4933675c213d6bd2b4
Dian Rahmaji <dianrahmaji@gmail.com>
2025-06-08 09:17:35 +0700

    feat: add `JobsService`

--- apps/jobber-jobs/src/app/jobs/Jobs.module.ts
+++ apps/jobber-jobs/src/app/jobs/Jobs.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FibonacciJob } from './Fibonacci.job';
+import { DiscoveryModule } from '@golevelup/nestjs-discovery';
+import { JobsService } from './Jobs.service';
 
 @Module({
-  providers: [FibonacciJob],
+  imports: [DiscoveryModule],
+  providers: [FibonacciJob, JobsService],
 })
 export class JobsModule {}
